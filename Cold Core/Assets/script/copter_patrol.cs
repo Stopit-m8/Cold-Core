081e1419816d34bb7745cb4f4b46f05998cabe2a
Stopit-m8 <sunelsensurya@gmail.com>
2025-02-04 20:51:07 +0700

    drone (WIP) (melayang pergi anjir)

--- Cold Core/Assets/script/copter_patrol.cs
+++ Cold Core/Assets/script/copter_patrol.cs
@@ -23,6 +23,53 @@ public class copter_patrol : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
+
+        if (HasLOS == true)
+        {
+            rb.velocity = new Vector2(0f, 0f);
+        }
+        else
+        {
+
+            Vector2 point = currentPoint.position - transform.position;
+            if (currentPoint == PointB.transform)
+            {
+                rb.velocity = new Vector2(0f, speed);
+
+            }
+            else
+            {
+                rb.velocity = new Vector2(0f, -speed);
+
+            }
+
+            if (Vector2.Distance(transform.position, currentPoint.position) < 0.5f && currentPoint == PointB.transform)
+            {
+                currentPoint = PointA.transform;
+            }
+            if (Vector2.Distance(transform.position, currentPoint.position) < 0.5f && currentPoint == PointA.transform)
+            {
+                currentPoint = PointB.transform;
+            }
+
+            //if (speed > 0.01)
+            //{
+            //    animator.SetBool("Speed", true);
+            //}
+            //else if (speed < 0)
+            //{
+            //    animator.SetBool("Speed", false);
+            //}
+        }
+
     }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(PointA.transform.position, 0.5f);
+        Gizmos.DrawWireSphere(PointB.transform.position, 0.5f);
+        Gizmos.DrawLine(PointA.transform.position, PointB.transform.position);
+    }
+
 }
