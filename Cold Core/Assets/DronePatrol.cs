485dcc33ae67becd8e8a8773bd9bd9171c030988
Stopit-m8 <sunelsensurya@gmail.com>
2025-02-05 18:58:39 +0700

    add drone (WIP)

---
+++ Cold Core/Assets/DronePatrol.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DronePatrol : MonoBehaviour
+{
+    public GameObject PointA;
+    public GameObject PointB;
+    public GameObject PointC;
+    private Rigidbody2D rb;
+    private Transform currentPoint;
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        currentPoint = PointA.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 point = currentPoint.position - transform.position;
+        if (currentPoint == PointA.transform)
+        {
+            rb.velocity = new Vector2(0f, speed);
+        }
+        else
+        {
+            rb.velocity = new Vector2(0f, -speed);
+        }
+        if (Vector2.Distance(transform.position, currentPoint.position) < 0.5f && currentPoint == PointA.transform)
+        {
+            currentPoint = PointB.transform;
+        }
+        if (Vector2.Distance(transform.position, currentPoint.position) < 0.5f && currentPoint == PointB.transform)
+        {
+            currentPoint = PointA.transform;
+        }
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(PointA.transform.position, 0.5f);
+        Gizmos.DrawWireSphere(PointB.transform.position, 0.5f);
+        Gizmos.DrawLine(PointA.transform.position, PointB.transform.position);
+    }
+}
