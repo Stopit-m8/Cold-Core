d4a5004d4d7ac0a7228a3c067a1ae65503ee0020
Stopit-m8 <sunelsensurya@gmail.com>
2025-02-06 15:19:52 +0700

    drone patrol complete

--- Cold Core/Assets/DronePatrol.cs
+++ Cold Core/Assets/DronePatrol.cs
@@ -20,28 +20,41 @@ public class DronePatrol : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //Vector3 direction = currentPoint.transform.position - transform.position;
         Vector2 point = currentPoint.position - transform.position;
         if (currentPoint == PointA.transform)
         {
-            rb.velocity = new Vector2(0f, speed);
+            
+            rb.velocity = new Vector2(point.x, point.y).normalized * speed;
+        }
+        else if (currentPoint == PointB.transform)
+        {
+            rb.velocity = new Vector2(point.x, point.y).normalized * speed;
         }
         else
         {
-            rb.velocity = new Vector2(0f, -speed);
+            rb.velocity = new Vector2(point.x, point.y).normalized * speed;
         }
         if (Vector2.Distance(transform.position, currentPoint.position) < 0.5f && currentPoint == PointA.transform)
         {
-            currentPoint = PointB.transform;
+            currentPoint = PointC.transform;
         }
         if (Vector2.Distance(transform.position, currentPoint.position) < 0.5f && currentPoint == PointB.transform)
         {
             currentPoint = PointA.transform;
+        }
+        if (Vector2.Distance(transform.position, currentPoint.position) < 0.5f && currentPoint == PointC.transform)
+        {
+            currentPoint = PointB.transform;
         }
     }
     private void OnDrawGizmos()
     {
         Gizmos.DrawWireSphere(PointA.transform.position, 0.5f);
         Gizmos.DrawWireSphere(PointB.transform.position, 0.5f);
+        Gizmos.DrawWireSphere(PointC.transform.position, 0.5f);
         Gizmos.DrawLine(PointA.transform.position, PointB.transform.position);
+        Gizmos.DrawLine(PointB.transform.position, PointC.transform.position);
+        Gizmos.DrawLine(PointC.transform.position, PointA.transform.position);
     }
 }
